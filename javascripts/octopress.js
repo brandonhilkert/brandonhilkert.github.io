00a999263d79e71daaab42ca19737004d583b87b
Brandon Hilkert <brandonhilkert@gmail.com>
2013-11-25 14:54:06 -0500

    Site updated at 2013-11-25 19:54:06 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -1,42 +1,3 @@
-function getNav() {
-  var mainNav = $('ul.main-navigation, ul[role=main-navigation]').before('<fieldset class="mobile-nav">')
-  var mobileNav = $('fieldset.mobile-nav').append('<select>');
-  mobileNav.find('select').append('<option value="">Navigate&hellip;</option>');
-  var addOption = function(i, option) {
-    mobileNav.find('select').append('<option value="' + this.href + '">&raquo; ' + $(this).text() + '</option>');
-  }
-  mainNav.find('a').each(addOption);
-  $('ul.subscription a').each(addOption);
-  mobileNav.find('select').bind('change', function(event) {
-    if (event.target.value) { window.location.href = event.target.value; }
-  });
-}
-
-function addSidebarToggler() {
-  if(!$('body').hasClass('sidebar-footer')) {
-    $('#content').append('<span class="toggle-sidebar"></span>');
-    $('.toggle-sidebar').bind('click', function(e) {
-      e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
-    });
-  }
-  var sections = $('aside.sidebar > section');
-  if (sections.length > 1) {
-    sections.each(function(index, section){
-      if ((sections.length >= 3) && index % 3 === 0) {
-        $(section).addClass("first");
-      }
-      var count = ((index +1) % 2) ? "odd" : "even";
-      $(section).addClass(count);
-    });
-  }
-  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
-}
-
 function testFeatures() {
   var features = ['maskImage'];
   $(features).map(function(i, feature) {
@@ -100,22 +61,11 @@ function wrapFlashVideos() {
   $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
 }
 
-function renderDeliciousLinks(items) {
-  var output = "<ul>";
-  for (var i=0,l=items.length; i<l; i++) {
-    output += '<li><a href="' + items[i].u + '" title="Tags: ' + (items[i].t == "" ? "" : items[i].t.join(', ')) + '">' + items[i].d + '</a></li>';
-  }
-  output += "</ul>";
-  $('#delicious').html(output);
-}
-
 $('document').ready(function() {
   testFeatures();
   wrapFlashVideos();
   flashVideoFallback();
   addCodeLineNumbers();
-  getNav();
-  addSidebarToggler();
 });
 
 // iOS scaling bug fix
--- javascripts/slash.js
+++ javascripts/slash.js
@@ -7,7 +7,7 @@
 			var href = this.href,
 				link = href.replace(/https?:\/\/([^\/]+)(.*)/, '$1');
 
-			if (link != '' && link != host && !$(this).hasClass('fancybox')){
+			if (link != '' && link != host){
 				window.open(href);
 				e.preventDefault();
 			}
@@ -33,30 +33,4 @@
 	externalLinks(); // Delete or comment this line to disable opening external links in new window
 	// appendCaption(); // Delete or comment this line to disable caption
 
-	var mobilenav = $('#mobile-nav');
-
-	$('html').click(function(){
-		mobilenav.find('.on').each(function(){
-			$(this).removeClass('on').next().hide();
-		});
-	});
-
-	mobilenav.on('click', '.menu .button', function(){
-		if (!$(this).hasClass('on')){
-			var width = $(this).width() + 42;
-			$(this).addClass('on').next().show().css({width: width});
-		} else {
-			$(this).removeClass('on').next().hide();
-		}
-	}).on('click', '.search .button', function(){
-		if (!$(this).hasClass('on')){
-			var width = mobilenav.width() - 51;
-			mobilenav.children('.menu').children().eq(0).removeClass('on').next().hide();
-			$(this).addClass('on').next().show().css({width: width}).children().children().eq(0).focus();
-		} else {
-			$(this).removeClass('on').next().hide().children().children().eq(0).val('');
-		}
-	}).click(function(e){
-		e.stopPropagation();
-	});
 })(jQuery);
