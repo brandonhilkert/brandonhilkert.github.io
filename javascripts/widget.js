f423c7ecb28b488ccfca7ebc0148407c98ebf60f
Brandon Hilkert <brandonhilkert@gmail.com>
2014-06-10 19:58:27 -0400

    Site updated at 2014-06-10 23:58:27 UTC

---
+++ javascripts/widget.js
@@ -0,0 +1,33 @@
+window.Widget = {
+  widget: $(".subscribe-widget"),
+  open: $(".subscribe-widget .open"),
+  close: $(".subscribe-widget .close"),
+
+  init: function(){
+    this.widget.fadeIn("slow");
+  },
+
+  show: function(){
+    this.widget.animate({ bottom: 0 });
+    this.open.fadeOut();
+    this.close.fadeIn();
+  },
+
+  hide: function(){
+    this.widget.animate({ bottom: "-220px" });
+    this.close.fadeOut();
+    this.open.fadeIn();
+  }
+}
+$(document).on("click", ".subscribe-widget .title, .subscribe-widget .open", function(e){
+  Widget.show();
+});
+
+$(document).on("click", ".subscribe-widget .close", function(e){
+  Widget.hide();
+});
+
+$(document).ready(function(){
+  Widget.init();
+});
+
